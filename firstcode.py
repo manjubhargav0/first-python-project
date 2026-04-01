e5333d3736b2adf1c07479630a22026b1df6f91b
manjubhragav <151710775+manjubhargav0@users.noreply.github.com>
2026-04-01 11:26:58 +0530

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,55 @@
+print("Hello World")
+print("manju bhargav")
+print("my age is 18")
+print(34+78)
+name = "manju bhargav"
+print(name)
+print(type(name))
+age = 18
+cgpa = 9.5
+print(type(age))
+print(type(cgpa))
+old = False
+love = None
+print(type(old))
+print(type(love))
+a = 99
+b = 2
+sum = a-b 
+# this is damn my frist code 
+"""manju is greatest"""
+    #arithmetic operations
+print(a % b)
+print(a**b)
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+num = 10
+num = num + 10
+print(num)
+num += 10
+num -= 10
+num *= 10
+num /= 10
+num %= 10
+num **= 10
+print("num", num)
+print(not False)
+val1 = True
+val2 = False
+first = int(input("enter your first value :", ))
+second = int(input("enter your sencond value :", ))
+sum1 = first + second
+print(sum1)
+#done 
+s1 = int(input("enter your frist side :", ))
+s2 = int(input("enter your another side :", ))
+print("area =", s1 * s2)
+a1 = float(input("enter your first value :", ))
+a2 = float(input("enter your second :", ))
+print("average =", (a1 +a2)/2)
+#done happy
+p = int(input("enter your value :", ))
+p2 = int(input("enter your second value :", ))
+print(p>=p2)
